2076d16c0278a653e903cdb54d9377ef11fefcba
Maryam Tamlalti <105081808+maryamtamlalti2001@users.noreply.github.com>
2024-09-08 02:41:41 +0200

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -9,12 +9,27 @@ class TestFlaskApp(TestCase):
     def test_get_request(self):
         response = self.client.get('/')
         self.assert200(response)
-        self.assertIn("Current POST requests count:", response.data.decode())
+        
+        json_data = response.get_json()
+
+    # Assert that the "counter" and "csrf_token" fields exist in the response
+        self.assertIn("counter", json_data)
+        self.assertIn("csrf_token", json_data)
+
 
     def test_post_request(self):
-        initial_count = read_counter()
-        response = self.client.post('/')
+    # Step 1: Get the CSRF token
+        response = self.client.get('/')
         self.assert200(response)
+        csrf_token = response.json["csrf_token"]
+
+    # Step 2: Use the CSRF token in the POST request
+        initial_count = read_counter()
+        response = self.client.post('/', headers={'X-CSRFToken': csrf_token})
+    
+        self.assert200(response)
+    
         updated_count = read_counter()
         self.assertEqual(updated_count, initial_count + 1)
-        self.assertIn(f"POST requests counter updated. Current count: {updated_count}", response.data.decode())
+        json_data = response.json
+        self.assertEqual(json_data["new_counter"], updated_count)
